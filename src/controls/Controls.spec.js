3b149fc26b974432130e08c68bd677439ae454da
Kevin Allen <kevin.allen91@gmail.com>
2019-10-14 13:45:39 -0700

    tests added

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent} from '@testing-library/react'
+
+import Controls from './Controls'
+
+test('Renders', () => {
+    render(<Controls />)
+})
+
