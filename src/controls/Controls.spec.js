a0aa955f2617a79b8758c35bbf14679852961967
Kevin Allen <kevin.allen91@gmail.com>
2019-10-14 15:04:03 -0700

    mvp complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -5,6 +5,14 @@ import { render, fireEvent} from '@testing-library/react'
 import Controls from './Controls'
 
 test('Renders', () => {
-    render(<Controls />)
+    expect(render(<Controls />)).toMatchSnapshot()
 })
 
+test('toggle lock', () => {
+    const toggleLocked = jest.fn()
+    const { getByText } = render(
+        <Controls toggleLocked={toggleLocked} />
+    )
+    fireEvent.click(getByText(/toggle locked/i))
+    expect(toggleLocked).toHaveBeenCalled()
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+
+import React from 'react'
+import Display from './Display'
+import { render, fireEvent } from '@testing-library/react'
+
+test('Renders', () => {
+    expect(render(<Display />)).toMatchSnapshot()
+})
+
+test('toggle lock', () => {
+    const toggleClosed = jest.fn()
+    const toggleLocked = jest.fn()
+    const { getByText } = render(
+        <Controls toggleLocked={toggleLocked} toggleClosed={toggleClosed} />
+    )
+    fireEvent.click(getByText(/toggle locked/i))
+    expect(toggleLocked).toHaveBeenCalled(true)
+
+    fireEvent.click(getByText(/toggle closed/i))
+    expect(toggleClosed).toHaveBeenCalled(true)
+})
